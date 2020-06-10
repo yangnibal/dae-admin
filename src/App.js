96c121958505c397f10d0801f0c822483fb24199
yangnibal <wonjun9670@naver.com>
2020-06-10 22:57:22 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import UserList from './pages/UserList'
 import UserUpdate from './pages/UserUpdate'
+import Inf from './pages/Inf';
+import VidList from './pages/VidList'
+import MatList from './pages/MatList'
+import NewMat from './pages/NewMat'
+import NewVid from './pages/NewVid'
+import NewInfGroup from './pages/NewInfGroup';
  
 class App extends React.Component{
 	render(){
@@ -14,6 +20,12 @@ class App extends React.Component{
 					<Route exact path="/account/login" component={Login}/>
 					<Route exact path="/users" component={UserList}/>
 					<Route exact path="/users/:username/update" component={UserUpdate}/>
+					<Route exact path="/inf" component={Inf}/>
+					<Route exact path="/inf/vid" component={VidList}/>
+					<Route exact path="/inf/mat" component={MatList}/>
+					<Route exact path="/inf/vid/new" component={NewVid}/>
+					<Route exact path="/inf/mat/new" component={NewMat}/>
+					<Route exact path="/inf/group/new" component={NewInfGroup}/>
 				</Switch>
 			</BrowserRouter>
 		)
