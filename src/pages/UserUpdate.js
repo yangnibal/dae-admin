3245c64fc7ca8bc5f04d00dc88ff3dfe79a31225
yangnibal <wonjun9670@naver.com>
2020-06-11 19:03:44 +0900

    no message

--- src/pages/UserUpdate.js
+++ src/pages/UserUpdate.js
@@ -50,7 +50,7 @@ class UserUpdate extends React.Component{
             }
         })
         .then(res => {
-            console.log(res)
+            this.props.history.goBack()
         })
         .catch(err => {
             alert("사용자 수정에 실패했습니다. 다시 시도해주시기 바랍니다.")
