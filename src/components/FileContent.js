cddd15df4bf30568bf2cbaabee6fd1cc47612948
yangnibal <wonjun9670@naver.com>
2020-08-03 09:12:15 +0900

    no message

--- src/components/FileContent.js
+++ src/components/FileContent.js
@@ -7,7 +7,7 @@ const FileContent = ({name, subject, grade, group, update, remove, seeFiles, lin
             <div className="filecontent-text">{subject}</div>
             <div className="filecontent-text">{grade}</div>
             <div className="filecontent-text">{group}</div>
-            <a target="blank" className="filecontent-text" href={`/web/viewer.html?file=${encodeURIComponent(link)}`}>자료 보기</a>
+            <a target="blank" className="filecontent-text" href={link.charAt(link.length - 1)==="f" ? `/web/viewer.html?file=${encodeURIComponent(link)}` : "https://docs.google.com/viewer?url=" + link}>자료 보기</a>
             <div className="filecontent-btns">
                 <div className="filecontent-btn" onClick={update}>수정&nbsp;</div>/
                 <div className="filecontent-btn" onClick={remove}>&nbsp;삭제</div>
